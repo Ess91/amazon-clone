04ea566c624ab27ad2338f77311d39d373365af7
Ess91 <eagyare91@gmail.com>
2020-09-26 17:37:23 +0100

    payment page

--- src/App.js
+++ src/App.js
@@ -8,55 +8,59 @@ import Login from "./Login";
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
 import Payment from "./Payment";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe(
+  "pk_test_51HVfe1JQibx0f1gvRGXyVSLqXYoHbyvXGtAIXkSYgkAMmFOjmAkh6zQbYcqpCpihwgi9pA0y6Ly9XQXhdrIgmlat00zftaM2uy"
+);
 
 function App() {
   const [{}, dispatch] = useStateValue();
 
   useEffect(() => {
-//Will only run once when the app component loads...
-auth.onAuthStateChanged(authUser => {
-  
+    //Will only run once when the app component loads...
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        //User just logged in / the user was logged in
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        //User is logged out
 
-  if (authUser) {
-//User just logged in / the user was logged in
-dispatch({
-  type: 'SET_USER',
-  user: authUser
-})
-  } else {
-    //User is logged out
-
-    dispatch({
-      type: 'SET_USER',
-      user: null
-    })
-  }
-})
-  }, [])
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
 
   return (
     //BEM
     <Router>
       <div className="app">
-      
         <Switch>
-
-        <Route path="/login">
+          <Route path="/login">
             <Login />
           </Route>
 
-        <Route path="/checkout">
-        <Header />
+          <Route path="/checkout">
+            <Header />
             <Checkout />
           </Route>
 
           <Route path="/payment">
-        <Header />
-        <Payment />
+            <Header />
+            <Elements stripe={promise}>
+            <Payment />
+            </Elements>
           </Route>
 
           <Route path="/">
-          <Header />
+            <Header />
             <Home />
           </Route>
         </Switch>
--- src/Payment.js
+++ src/Payment.js
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Payment.css";
 import { useStateValue } from "./StateProvider";
 import CheckoutProduct from "./CheckoutProduct";
+import { Link } from "react-router-dom";
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 
 function Payment() {
   const [{ basket, user }, dispatch] = useStateValue();
+ 
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const [error, setError] = useState(null);
+  const [disabled, setDisabled] = useState(true);
+
+  const handleSubmit = e => {
+    //stripe part
+  }
+
+  const handleChange = e => {
+    //Listen for changes in CardElement
+    //Display any errors as the customer types their info
+  }
 
   return (
     <div className="payment">
       <div className="payment__container">
+        <h1>
+          Checkout (<Link to="/checkout">{basket?.length} items</Link>){" "}
+        </h1>
 
-          {/*Payment Section - Delivery Address*/}
+        {/*Payment Section - Delivery Address*/}
         <div className="payment__section">
           <div className="payment__title">
             <h3>Delivery Address</h3>
@@ -22,26 +42,38 @@ function Payment() {
           </div>
         </div>
 
-     {/*Payment Section - Review Items*/}
+        {/*Payment Section - Review Items*/}
         <div className="payment__section">
-            <div className="payment__title">
-                <h3>Review Items and Delivery</h3>
-            </div>
-            <div className="payment__items">
-                    {basket.map(item => (
-                        <CheckoutProduct 
-                        id={item.id}
-                        title={item.title}
-                        image={item.image}
-                        price={item.price}
-                        rating={item.rating}
-                        />
-                    ))}
-            </div>
+          <div className="payment__title">
+            <h3>Review Items and Delivery</h3>
+          </div>
+          <div className="payment__items">
+            {basket.map((item) => (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+            ))}
+          </div>
         </div>
 
-          {/*Payment Section - Payment Method*/}
-        <div className="payment__section"></div>
+        {/*Payment Section - Payment Method*/}
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Payment Method</h3>
+          </div>
+          <div className="payment__details">
+            {/*Stripe*/}
+            
+            <form onSubmit={handleSubmit}>
+
+              <CardElement onChange={handleChange}/>
+            </form>
+            </div>
+        </div>
       </div>
     </div>
   );

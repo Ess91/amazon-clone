399876d1a05560af8b749a3e3fd496c2bec60c86
Ess91 <eagyare91@gmail.com>
2020-09-27 00:34:44 +0100

    order styling

--- src/Orders.js
+++ src/Orders.js
@@ -1,35 +1,43 @@
-import React, { useState } from "react";
-import "./Orders.css";
+import React, { useState, useEffect } from 'react';
+import { db } from "./firebase";
+import './Orders.css'
+import { useStateValue } from "./StateProvider";
+import Order from './Order'
 
 function Orders() {
-    const [{ basket, user }, dispatch] = useStateValue([]);
+  const [{ basket, user }, dispatch] = useStateValue();
+  const [orders, setOrders] = useState([]);
 
-    useEffect(() => {
+  useEffect(() => {
+    if(user) {
         db
         .collection('users')
         .doc(user?.uid)
         .collection('orders')
         .orderBy('created', 'desc')
-        .onSnapshot(sbapshot => (
-            setOrders(snapshot.doc.map(doc => ({
+        .onSnapshot(snapshot => (
+            setOrders(snapshot.docs.map(doc => ({
                 id: doc.id,
                 data: doc.data()
             })))
         ))
+    } else {
+        setOrders([])
+    }
 
-    }, [])
+  }, [user])
 
-  return (
-    <div className="orders">
-      <h1>Your Orders</h1>
+    return (
+        <div className='orders'>
+            <h1>Your Orders</h1>
 
-     {/* <div className="orders__order">
-        {orders?.map((order) => (
-          <Order order={order} />
-        ))}
-      </div>*/} 
-    </div>
-  );
+            <div className='orders__order'>
+                {orders?.map(order => (
+                    <Order order={order} />
+                ))}
+            </div>
+        </div>
+    )
 }
 
-export default Orders;
+export default Orders

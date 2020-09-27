68de3d3e051b108a17a5babc5860601a5cf372da
Ess91 <eagyare91@gmail.com>
2020-09-27 16:34:36 +0100

    debugging

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5001/clone-56b93/us-central1/api'  // The API (cloud function) URL
+    baseURL: '...'  // The API (cloud function) URL
 });
 
-
+//http://localhost:5001/clone-56b93/us-central1/api
 export default instance;

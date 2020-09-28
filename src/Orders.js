4e12d23c8f994b8b3f48f705ca0779f740cda78f
Ess91 <eagyare91@gmail.com>
2020-09-28 12:03:54 +0100

    small changes

--- src/Orders.js
+++ src/Orders.js
@@ -32,7 +32,7 @@ function Orders() {
 
     return (
         <div className='orders'>
-            <h1>Your Orders</h1>
+            <h1>My Orders...</h1>
 
             <div className='orders__order'>
                 {orders?.map(order => (
